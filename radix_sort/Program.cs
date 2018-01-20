96e579860e9493bdd4ede21b236694a058ef19b7
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-20 11:41:24 +1100

    Add class Example

--- radix_sort/Program.cs
+++ radix_sort/Program.cs
@@ -14,6 +14,26 @@ namespace radix_sort
         private IList<IList<int>> digits = new List<IList<int>>();
         private int maxLength = 0;
         public Example()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                digits.Add(new List<int>());
+            }
+            Console.Write("Enter the Number of Records : ");
+            int count = int.Parse(Console.ReadLine());
+            data = new int[count];
+            Console.ReadLine();
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Enter Record {0} : ", i + 1);
+
+                data[i] = int.Parse(Console.ReadLine());
+
+                if (maxLength < data[i].ToString().Length)
+                    maxLength = data[i].ToString().Length;
+            }
+        }
+
 
 
         static void Main(string[] args)
